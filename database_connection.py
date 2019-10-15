edb74a07dab54d8b98edb82f457a4b66783c8d8c
humanoiA <shubhang.sharma123@live.com>
2019-10-15 18:14:16 +0530

    return_2_col

--- database_connection.py
+++ database_connection.py
@@ -9,10 +9,22 @@ def connect(column_name,table_name):
     charset='latin1',
     use_unicode=True
     )
-    
     mycursor = mydb.cursor(buffered=True)
-    c=0
     mycursor.execute('select {} from {}'.format(column_name,table_name))
     row = [item[0] for item in mycursor.fetchall()]
+    mydb.close()
+    return row
+def connect_2_col(column_name,table_name):
+    mydb = mysql.connector.connect(
+    host="frombckup.clb8pwlmio9e.ap-south-1.rds.amazonaws.com",
+    database="atgworld_p1036",
+    user="root",
+    passwd="8nRy7EbcMn5r",
+    charset='latin1',
+    use_unicode=True
+    )
+    mycursor = mydb.cursor(buffered=True)
+    mycursor.execute('select {} from {}'.format(column_name,table_name))
+    row = [[str(item[0]),str(item[1])] for item in mycursor.fetchall()]
     mydb.close()
     return row
